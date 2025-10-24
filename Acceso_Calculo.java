973010155c785a7a9fe1459492aba41d377edc6d
yi285 <Angellopez2006.24@gmail.com>
2025-10-24 21:01:49 +0200

    Actualizacion

--- Acceso_Calculo.java
+++ Acceso_Calculo.java
@@ -1,7 +1,7 @@
 public class Acceso_Calculo {
     public static void main(String[] args) {
        System.out.println("Acceso_Calculo main method executed.");
-
+        System.err.println("Error output from Acceso_Calculo.");
 
     }
 }
